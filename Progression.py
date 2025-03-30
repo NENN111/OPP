0ff42f29143e21033dd9ad2837b4179494a52948
NENN111 <g.platonova83@gmail.com>
2025-03-30 21:22:34 +0300

    lastversion

--- Progression.py
+++ Progression.py
@@ -1,5 +1,31 @@
 class ArithmeticProgression:
     def __init__(self,a0,q):
+        if q == 0:
+            raise ValueError("Коэффициент не может быть равен нулю.")
         self.a0 = a0
         self.q = q
-   
+        self.n = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        temp = self.a0+self.q*self.n
+        self.n+=1
+        return temp
+class GeometricProgression(ArithmeticProgression):
+    def __init__(self, a0, q):
+        super().__init__(a0, q)
+        self.n = 0 
+        
+
+    def __next__(self):
+        temp = self.a0 * (self.q ** self.n) 
+        self.n += 1
+        return temp
+        
+#пример использования
+progression = GeometricProgression(1, 2)
+
+for elem in progression:
+    if elem > 10:
+        break
+    print(elem, end=' ')    # 1 2 4 8
